8a68470d6ce179988404b777355a936282104916
Tam O'Donnell <47383699+ThomasHAOD@users.noreply.github.com>
2023-11-14 14:02:59 +0000

    update bun api

--- bun-api/index.ts
+++ bun-api/index.ts
@@ -1,10 +1,10 @@
 import { Client } from 'pg'
 const client = new Client({
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
+   user: process.env.PGUSER,
+   password: process.env.PGPASSWORD,
+   database: process.env.PGDATABASE,
+   host: process.env.PGHOST,
+   port: 54321,
 })
 await client.connect()
 
@@ -13,10 +13,19 @@ const server = Bun.serve({
    port: 3000,
    async fetch(req) {
       const url = new URL(req.url);
-      if (url.pathname === "/") {
-         return new Response("Hello World!");
+      if (url.pathname === "/echo" && req.method === "GET") {
+         const params = url.searchParams;
+         const body = JSON.stringify(params)
+
+         return new Response(body);
       }
-      if (url.pathname === "/drivers") {
+      if (url.pathname === '/echo' && req.method === "POST") {
+         const reqBody = await req.json();
+         const resBody = JSON.stringify(reqBody)
+
+         return new Response(resBody);
+      }
+      if (url.pathname === "/etl") {
          const { rows } = await client.query('SELECT * FROM drivers')
          const json = JSON.stringify(rows);
          return new Response(json);
--- deno-api/index.ts
+++ deno-api/index.ts
@@ -1,18 +1,28 @@
 import { serve } from "https://deno.land/std@0.206.0/http/server.ts";
 import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
 const client = new Client({
-   user: 'user',
-   password: 'password',
-   database: 'database',
-   hostname: 'localhost',
+   user: Deno.env.get('PGUSER'),
+   password: Deno.env.get('PGPASSWORD'),
+   database: Deno.env.get('PGDATABASE'),
+   hostname: Deno.env.get('PGHOST'),
    port: 54321,
 })
 await client.connect()
 const handler = async (request: Request): Promise<Response> => {
+   console.log(request.url);
+
    const url = new URL(request.url);
 
    if (url.pathname === "/") {
       return new Response("Hello World!");
+   }
+   if (url.pathname === "/echo") {
+      console.log(url);
+
+      const params = new URLSearchParams(url.search);
+
+      const body = JSON.stringify(params)
+      return new Response(body);
    }
 
    if (url.pathname === "/drivers") {
@@ -23,4 +33,4 @@ const handler = async (request: Request): Promise<Response> => {
    return new Response("Not Found", { status: 404 });
 };
 
-serve(handler);
+serve(handler, { port: 3002 });
