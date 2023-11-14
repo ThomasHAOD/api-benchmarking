4df9ba99157922fb44a3730b53f51cb1fad5a841
Ivo Genev <ivo.genev@and.digital>
2023-11-14 13:15:12 +0000

    fix node api and conform it to test scenario requirements

--- node-api/index.js
+++ node-api/index.js
@@ -1,4 +1,5 @@
 const http = require('node:http');
+const urlLib = require('url');
 const { Client } = require('pg');
 
 const client = new Client({
@@ -16,27 +17,33 @@ const hostname = 'localhost';
 const port = 3001;
 
 const server = http.createServer(async (req, res) => {
-  const { url } = req;
-  console.log('request recieved');
-  if (url === '/drivers') {
+  const { pathname, query } = urlLib.parse(req.url, true);
+  if (pathname === '/etl') {
     const { rows } = await client.query('SELECT * FROM drivers');
     const json = JSON.stringify(rows);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     res.end(json);
   }
-  if (url === '/') {
+  if (pathname === '/echo' && req.method === 'GET') {
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('Hello World!');
+    res.setHeader('Content-Type', 'application/json');
+    res.end(Buffer.from(JSON.stringify(query)));
   }
-  if (url !== '/drivers' && url !== '/') {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('Not Found');
+  if (pathname === '/echo' && req.method === 'POST') {
+		let body = '';
+		req.on('data', (data) => {
+			body += data;
+		})
+		req.on('end', () => {
+			res.statusCode = 200;
+			const bodyData = Buffer.from(body);
+			res.setHeader('Content-Type', 'application/json');
+			res.end(bodyData);
+		})
   }
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
