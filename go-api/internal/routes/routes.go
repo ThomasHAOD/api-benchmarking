4b90e626bbd6fb8b3820038756e1c9ac373f57a9
Murray.Stewart <Murray.stewart@and.digital>
2023-11-14 14:10:45 +0000

    feat: modified go routes

--- go-api/internal/routes/routes.go
+++ go-api/internal/routes/routes.go
@@ -7,13 +7,13 @@ import (
 )
 
 func AddBenchmarkRoutes(rg *gin.RouterGroup) {
-	bm := rg.Group("/benchmark")
-	bm.GET("/", handlers.GetEcho)
-	bm.POST("/", handlers.PostEcho)
-	bm.GET("/drivers", handlers.GetDrivers)
+	bm := rg.Group("")
+	bm.GET("/echo", handlers.GetEcho)
+	bm.POST("/echo", handlers.PostEcho)
+	bm.GET("/etl", handlers.GetDrivers)
 }
 
 func InitRoutes(r *gin.Engine) {
-	v1 := r.Group("/v1")
+	v1 := r.Group("")
 	AddBenchmarkRoutes(v1)
 }
